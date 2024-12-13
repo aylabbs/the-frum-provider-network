33915262142b541418ee835509a365e5e11f5e17
Abraham Labkovsky <aylabbs@gmail.com>
2024-12-13 05:58:19 +0000

    stop tracking env; setup backend to fetch providers

--- netlify/functions/data.mjs
+++ netlify/functions/data.mjs
@@ -1,15 +1,51 @@
-import contacts from './contacts';
+const BASE_ID = "appbPGcBMTauJAj18";
+const AIRTABLE_API_KEY = Netlify.env.get("AIRTABLE_API_KEY") || "";
+
+function tableResponseToObject(tableData) {
+  return tableData.records.reduce(
+    (acc, record) => ({ ...acc, [record.id]: record.fields.Name }),
+    {}
+  );
+}
+async function getAirtableTable(tableName) {
+  const url = `https://api.airtable.com/v0/${BASE_ID}/${encodeURIComponent(
+    tableName
+  )}`;
+  const response = await fetch(url, {
+    headers: { authorization: `Bearer ${AIRTABLE_API_KEY}` },
+  });
+  return await response.json();
+}
+const tableNames = ["Providers", "Service Areas", "Referrers", "Categories"];
 
 export default async function data(req, context) {
-  const message = Netlify.env.get("MESSAGE") || "default message";
-  
-  return new Response(
-    JSON.stringify({ contacts, message }), // JSON response body
-    {
-      headers: {
-        "Content-Type": "application/json", // Set the response type
-      },
-      status: 200, // HTTP status code
-    }
-  );
+  const promises = tableNames.map(getAirtableTable);
+  const [
+    providersResponse,
+    serviceAreasResponse,
+    referrers,
+    categoriesResponse,
+  ] = await Promise.all(promises);
+  const categories = tableResponseToObject(categoriesResponse);
+  const serviceAreas = tableResponseToObject(serviceAreasResponse);
+
+  const providers = providersResponse.records.map((provider) => ({
+    id: provider.id,
+    Phone: provider.fields["Phone"],
+    Categories: provider.fields["Categories"].map((id) => categories[id]),
+    lastName: provider.fields["Last Name"],
+    Email: provider.fields["Email"],
+    serviceAreas: provider.fields["Service Areas"].map(
+      (id) => serviceAreas[id]
+    ),
+    firstName: provider.fields["First Name"],
+    Company: provider.fields["Company"],
+  }));
+
+  return new Response(JSON.stringify({ providers }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 200,
+  });
 }
